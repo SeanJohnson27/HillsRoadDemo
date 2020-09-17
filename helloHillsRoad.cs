ebb2d2cc87da97a241c7cfa37c56044225a019e7
SeanJohnson27 <71438021+SeanJohnson27@users.noreply.github.com>
2020-09-17 11:41:13 +0100

    HillsRoadHelloStartingCode

---
+++ helloHillsRoad.cs
@@ -0,0 +1,12 @@
+using System;
+// Create a Hello Hills Road program in GitHub
+namespace Hello
+{
+  class HelloHillsRoad
+  {
+      static void Main()
+      {
+        System.WriteLine("Hello Hills Road!");
+      }
+  }
+}
